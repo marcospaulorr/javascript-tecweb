bde3dbe4433aa8182b923af5d8e6b5f1e5db77db
marcospaulorr <marcospauloricartereboucas@gmail.com>
2024-04-09 18:27:26 -0300

    exercicio 2

--- client.js
+++ client.js
@@ -1,4 +1,12 @@
 const axios = require("axios");
-axios
-  .get("https://catfact.ninja/fact")
-  .then((response) => console.log(response.data.fact));
+
+const username = "marcosprr"
+
+axios.post('https://tecweb-js.insper-comp.com.br/token', {username: username},{
+    headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}}).then(response => {
+    const accessToken = response.data.accessToken;
+    console.log("Token de acesso:", accessToken);
+}).catch(error => console.error("Erro na requisição:", error))
