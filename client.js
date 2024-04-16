e69f0d0cb181032bb256775728a210b1d7b86855
marcospaulorr <marcospauloricartereboucas@gmail.com>
2024-04-16 11:14:26 -0300

    exercciciso enviados

--- client.js
+++ client.js
@@ -1,62 +1,64 @@
 const axios = require("axios");
 
-const username = "marcosprr"
+const config = {
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+}
 
 function getToken(){
-    return axios.post('https://tecweb-js.insper-comp.com.br/token', {username: username},{
-        headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
+    return axios
+        .post("https://tecweb-js.insper-comp.com.br/token", { username: "marcosprr" }, config)
+        .then((response) => response.data.accessToken);
 }
 
-function listaExercicios(accessToken){
-    return axios.get('https://tecweb-js.insper-comp.com.br/exercicio', {
-        headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
+function listaExercicios(token){
+    config.headers.Authorization = `Bearer ${token}`;
+    return axios
+        .get("https://tecweb-js.insper-comp.com.br/exercicio", config)
+        .then((response) => response.data)
 }
 
-function enviarResposta(accessToken,slug,resposta){
-    return axios.post(`https://tecweb-js.insper-comp.com.br/exercicio/${slug}`, {resposta: resposta},{
-        headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
-}
 
-getToken().then(response => {
-    const accessToken = response.data.accessToken;
+let exercicios;
+getToken().then(accessToken => {
     console.log("Token de acesso:", accessToken);
 
     return listaExercicios(accessToken);
 }).then(response => {
-    console.log("Lista de exercicios:", response.data);
+    exercicios = response; 
+    console.log("Lista de exercicios:", response);
 }).catch(error => console.error("Erro na requisição:", error));
 
-function somaValores(entrada){
-    return entrada.a + entrada.b;
+async function postExercicio(token, exercicioId, entrada){
+    config.headers.Authorization = `Bearer ${token}`;
+    try {
+        const response = await axios.post(`https://tecweb-js.insper-comp.com.br/exercicio/${exercicioId}`, entrada, config);
+        return response.data;
+    } catch (error) {
+        console.error(`Erro ao enviar POST para ${exercicioId}:`, error.response ? error.response.data : error.message);
+        return null;
+    }
 }
 
-function tamanhoString(entrada){
-    return entrada.string.length;
+function somaValores(a,b){
+    return a + b;
 }
 
-function nomeUsuario(entrada){
-    return entrada.email.split('@')[0];
+
+function tamanhoString(string){
+    return string.length;
 }
 
-function jacaWars(entrada){
+
+function nomeUsuario(email){
+    return email.split('@')[0];
+}
+
+function jacaWars(v, theta){
     const g = 9.8;
-    const theta = entrada.theta;
     const radianos = theta*Math.PI/180;
-    const v = entrada.v;
     const distancia = (v*v*Math.sin(2*radianos))/g;
 
     if (distancia > 102){
@@ -68,8 +70,7 @@ function jacaWars(entrada){
     }
 }
 
-function anoBissexto(entrada){
-    const ano = entrada.ano;
+function anoBissexto(ano){
     if ((ano % 4 === 0 && ano % 100 !== 0) || ano % 400 ===0){
         return true;
     }else{
@@ -77,24 +78,17 @@ function anoBissexto(entrada){
     }
 }
 
-function volumePizza(entrada){
-    const pi = Math.pi;
-    const r = entrada.z;
-    const h = entrada.a;
-    const volume = pi*r*r*a;
+function volumePizza(z,a){
+    const volume = Math.PI*z**2*a;
     return Math.round(volume);
 }
 
-function mru(entrada){
-    const s0 = entrada.s0;
-    const v = entrada.v;
-    const t = entrada.t;
+function mru(s0,v,t){
     const s = s0 + v*t;
     return s;
 }
 
-function inverteString(entrada){
-    const string = entrada.string;
+function inverteString(string){
     return string.split('').reverse().join('');
 }
 
@@ -133,20 +127,21 @@ function nPrimo(n){
 }
 
 function maiorPrefixoComum(strings){
-    if (strings.length === 0) return '';
+    if (strings.length === 0) return ''; // Retorna uma string vazia se o array de strings estiver vazio
 
     let prefixo = '';
 
-    for (let i = 0; i<strings[0].length; i++){
-        const char = strings[0][i];
-        for (let j=1; j<strings.length;j++){
-            if (i === strings[j].length || strings[j][i] !== char){
-                return prefixo;
+    for (let i = 0; i < strings[0].length; i++){ // Itera sobre cada caractere da primeira string
+        const char = strings[0][i]; // Armazena o caractere atual da primeira string para comparação
+
+        for (let j = 1; j < strings.length; j++){ // Compara o caractere com o mesmo índice nas outras strings
+            if (i === strings[j].length || strings[j][i] !== char){ // Checa se alcançou o fim de qualquer string ou se os caracteres diferem
+                return prefixo; // Retorna o prefixo comum encontrado até agora
             }
         }
-        prefixo += char;
+        prefixo += char; // Se o caractere é comum a todas as strings, adiciona ao prefixo
     }
-    return prefixo;
+    return prefixo; // Retorna o prefixo comum máximo encontrado
 }
 
 function somaSegundoMaiorMenor(numeros){
@@ -207,3 +202,105 @@ async function cacarTesouro(urlInicial){
     }
 }
 
+
+async function main(){
+    let token = await getToken();
+    if (!token) {
+        console.log("Token não recebido, não é possível continuar.");
+        return;
+    }
+
+    const exercicios = await listaExercicios(token);
+    if (!exercicios) {
+        console.log("Não foi possível obter a lista de exercícios.");
+        return;
+    }
+
+
+    let n1 = exercicios.soma.entrada.a;
+    let n2 = exercicios.soma.entrada.b;
+    let resultado = somaValores(n1,n2);
+    const response1 = await postExercicio(token, 'soma', { resposta: resultado });
+    console.log("Resposta do servidor para soma:", response1);
+
+    let string = exercicios['tamanho-string'].entrada.string;
+    let resultado2 = tamanhoString(string);
+    const response2 = await postExercicio(token, 'tamanho-string', { resposta: resultado2 });
+    console.log("Resposta do servidor para tamanho-string:", response2);
+    
+    let email = exercicios['nome-do-usuario'].entrada.email;
+    let resultado3 = nomeUsuario(email);
+    const response3 = await postExercicio(token, 'nome-do-usuario', { resposta: resultado3 });
+    console.log("Resposta do servidor para nome-do-usuario:", response3);
+
+    let jaca_v = exercicios['jaca-wars'].entrada.v;
+    let jaca_theta = exercicios['jaca-wars'].entrada.theta;
+    let resultado4 = jacaWars(jaca_v, jaca_theta);
+    const response4 = await postExercicio(token, 'jaca-wars', { resposta: resultado4 });
+    console.log("Resposta do servidor para jaca-wars:", response4);
+
+    let ano = exercicios['ano-bissexto'].entrada.ano;
+    let resultado5 = anoBissexto(ano);
+    const response5 = await postExercicio(token, 'ano-bissexto', { resposta: resultado5 });
+    console.log("Resposta do servidor para ano-bissexto:", response5);
+    
+    let z = exercicios['volume-da-pizza'].entrada.z;
+    let a = exercicios['volume-da-pizza'].entrada.a;
+    let resultado6 = volumePizza(z,a);
+    const response6 = await postExercicio(token, 'volume-da-pizza', { resposta: resultado6 });
+    console.log("Resposta do servidor para volume-da-pizza:", response6);
+
+    let mru_s0 = exercicios['mru'].entrada.s0;
+    let mru_v = exercicios['mru'].entrada.v;
+    let mru_t = exercicios['mru'].entrada.t;
+    let resultado7 = mru(mru_s0, mru_v, mru_t);
+    const response7 = await postExercicio(token, 'mru', { resposta: resultado7 });
+    console.log("Resposta do servidor para mru:", response7);
+
+    let inverte_string = exercicios['inverte-string'].entrada.string;
+    let resultado8 = inverteString(inverte_string);
+    const response8 = await postExercicio(token, 'inverte-string', { resposta: resultado8 });
+    console.log("Resposta do servidor para inverte-string:", response8);
+
+    let lista_soma = exercicios['soma-valores'].entrada.objeto;
+    let resultado9 = somaValoresObjeto(lista_soma);
+    const response9 = await postExercicio(token, 'soma-valores', { resposta: resultado9 });
+    console.log("Resposta do servidor para soma-valores:", response9);
+
+    let n = exercicios['n-esimo-primo'].entrada.n;
+    let resultado10 = nPrimo(n);
+    const response10 = await postExercicio(token, 'n-esimo-primo', { resposta: resultado10 });
+    console.log("Resposta do servidor para n-esimo-primo:", response10);
+
+    let lista_strings = exercicios['maior-prefixo-comum'].entrada.strings;
+    let resultado11 = maiorPrefixoComum(lista_strings);
+    const response11 = await postExercicio(token, 'maior-prefixo-comum', { resposta: resultado11 });
+    console.log("Resposta do servidor para maior-prefixo-comum:", response11);
+
+    let lista_palavras = exercicios['conta-palindromos'].entrada.palavras;
+    let resultado12 = contaPalindromos(lista_palavras);
+    const response12 = await postExercicio(token, 'conta-palindromos', { resposta: resultado12 });
+    console.log("Resposta do servidor para conta-palindromos:", response12);
+
+    let soma_strings = exercicios['soma-de-strings-de-ints'].entrada.strings;
+    let resultado13 = somaStrings(soma_strings);
+    const response13 = await postExercicio(token, 'soma-de-strings-de-ints', { resposta: resultado13 });
+    console.log("Resposta do servidor para soma-de-strings-de-ints:", response13);
+
+    let endpoints = exercicios['soma-com-requisicoes'].entrada.endpoints;
+    let resultado14 = somaComRequisicoes(endpoints);
+    const response14 = await postExercicio(token, 'soma-com-requisicoes', { resposta: resultado14 });
+    console.log("Resposta do servidor para soma-com-requisicoes:", response14);
+
+    let url0 = exercicios['caca-ao-tesouro'].entrada.inicio;
+    let resultado15 = cacarTesouro(url0);
+    const response15 = await postExercicio(token, 'caca-ao-tesouro', { resposta: resultado15 });
+    console.log("Resposta do servidor para caca-ao-tesouro:", response15);
+
+    let lista_numeros = exercicios['soma-segundo-maior-e-menor-numeros'].entrada.numeros;
+    let resultado16 = somaSegundoMaiorMenor(lista_numeros);
+    const response16 = await postExercicio(token, 'soma-segundo-maior-e-menor-numeros', { resposta: resultado16 });
+    console.log("Resposta do servidor para soma-segundo-maior-e-menor-numeros:", response16);
+}
+
+main();
